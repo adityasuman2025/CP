ee0343969d0a9661e8c7d2a230ba5dff4298f02a
adityasuman2025 <adityasuman2025@gmail.com>
2018-09-06 00:33:43 +0530

    sumLimit solved

--- sumLimit.py
+++ sumLimit.py
@@ -7,19 +7,20 @@ for i in range(N):
     data_org = data.copy()
     
     lenn = int(len(data))
-   
+    
     for j in range(1, lenn):
-        data[j] = data[j-1]*data[j]
+        data[j] = data[j-1]+data[j]
         
     #print(data)
 
 #tym limit excceded    
-    count = 0
+    count = []
     for j in range(lenn):
-        divisor = 1 if j==0 else data[j-1]
+        subs = 0 if j==0 else data[j-1]
         for k in range(j, lenn):
-            if data[k]/divisor < pro:
-                count +=1
-            else:
-                break
-    print(count)
+            if data[k]-subs > pro:
+                count.append(k-j+1)
+                break;
+                
+    
+    print(min(count))
