53971d339772d1a10e4eff4294f7a455ffc123f6
adityasuman2025 <adityasuman2025@gmail.com>
2018-09-18 23:36:11 +0530

    10

---
+++ Assignment_10/Assignment_10_5_1601me01.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Assignment_10_5_1601me01
+{	
+	Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		Stack<Character> st = new Stack<>();
+
+		String str = scanner.nextLine();
+
+		for(int i =0; i<str.length(); i++)
+		{
+			char val = str.charAt(i);
+
+			if(val == '[' || val == '(' || val == '{')
+			{
+				st.push(val);
+			}
+
+			if(val == ')')
+			{
+				char temp = st.peek();
+				if(temp == '(')
+				{
+					st.pop();
+				}
+			}
+			else if(val == '}')
+			{
+				char temp = st.peek();
+				if(temp == '{')
+				{
+					st.pop();
+				}
+			}
+			else if(val == ']')
+			{
+				char temp = st.peek();
+				if(temp == '[')
+				{
+					st.pop();
+				}
+			}
+		}
+
+		System.out.prinln(st);
+		if(st.isEmpty())
+		{	
+			System.out.println("The expression is having balanced symbol");
+		}
+		else
+		{
+			System.out.println("The expression is not balanced");	
+		}
+	}
+}
