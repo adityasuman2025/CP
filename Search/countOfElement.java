48bdf9c93de2192a65288634ff6a7762df37b0ee
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-08-08 09:11:35 +0530

    search prblms

---
+++ Search/countOfElement.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+class countOfElement
+{
+	public static int bs(int arr[], int start, int end, int to_find, boolean searchFirst)
+	{
+		int result = -1;		
+
+		while(start<=end)
+		{
+			int mid = start + (end-start)/2;
+
+			if(to_find == arr[mid])
+			{
+				result = mid;
+
+				if(searchFirst) //searching for first index;
+				{
+					end = mid-1;
+				}
+				else
+				{
+					start = mid+1;
+				}			
+			}
+			else if(to_find<arr[mid])
+			{
+				end = mid-1;
+			}
+			else
+			{
+				start = mid+1;
+			}
+		}		
+
+		return result;
+	}
+
+	public static int count(int arr[], int to_find)
+	{
+		int n = arr.length;
+
+		int firstIndex = bs(arr, 0, n-1, to_find, true);
+
+		if(firstIndex != -1)
+		{
+			int lastIndex = bs(arr, 0, n-1, to_find, false);	
+		
+			return lastIndex-firstIndex+1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		int arr[] = {9,52,52,52,2,0,52, 2, 100, 71, 71};
+		Arrays.sort(arr);
+		System.out.println("Given array is: " + Arrays.toString(arr));	
+
+		int to_find = 10;
+		System.out.println("Count of " + to_find +" is: " + count(arr, to_find));	
+	}
+}
