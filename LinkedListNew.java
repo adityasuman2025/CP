f478f88d7af46e856ea40d94c7f3822420ec8d2a
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-08-09 23:52:55 +0530

    rotatin of linked lis

--- LinkedListNew.java
+++ LinkedListNew.java
@@ -94,6 +94,124 @@ public class LinkedListNew
 		return false;
 	}	
 
+//to rotate linked list in left direction
+	void rotateLeft(int k)
+	{
+		NodeNew current2 = head;
+		NodeNew current = head;
+
+	//getting size of the linked list
+		int size = 0;
+
+		if(current2 == null)
+			size = 0;
+
+		while(current2.next !=null)
+		{
+			current2 = current2.next;
+			size++;
+		}
+		size++;		
+
+		if(k>=size)
+			k = k%size;
+
+		if(k == 0)
+			return;
+
+	//going to the kth node	
+		int count = 1;
+		while(current != null)
+		{
+			if(count<k)
+			{
+				current = current.next;
+				count++;
+			}
+			else
+			{
+				break;
+			}			
+		}
+
+		if(current == null) //when k is greater or equal to size of linked list
+			return;
+
+		NodeNew kthNode = current;
+
+	//going to the last element of the linked list and linking last element to head
+		while(current.next !=null)
+		{
+			current = current.next;
+		}
+		current.next = head;
+
+	// making the element next to the kth element as head 	
+		head = kthNode.next;
+
+	//making next of the kth element null
+		kthNode.next = null;
+	}
+
+//to rotate linked list in right direction
+	void rotateRight(int k)
+	{		
+		NodeNew current2 = head;
+		NodeNew current = head;
+
+	//getting size of the linked list
+		int size = 0;
+
+		if(current2 == null)
+			size = 0;
+
+		while(current2.next !=null)
+		{
+			current2 = current2.next;
+			size++;
+		}
+		size++;		
+
+		if(k>=size)
+			k = k%size;
+
+		if(k == 0)
+			return;
+		
+	//going to the kth node	
+		int count = 1;
+		while(current != null)
+		{
+			if(count<size-k)
+			{
+				current = current.next;
+				count++;
+			}
+			else
+			{
+				break;
+			}			
+		}
+
+		if(current == null) //when k is greater or equal to size of linked list
+			return;
+
+		NodeNew kthNode = current;
+
+	//going to the last element of the linked list and linking last element to head
+		while(current.next !=null)
+		{
+			current = current.next;
+		}
+		current.next = head;
+
+	// making the element next to the kth element as head 	
+		head = kthNode.next;
+
+	//making next of the kth element null
+		kthNode.next = null;
+	}
+
 //to print the linkedlist
 	public void print()
 	{
@@ -107,10 +225,12 @@ public class LinkedListNew
 
 		while(current.next != null)
 		{
-			System.out.println(current.data);
+			System.out.print(current.data + " ");
 			current = current.next;
 		}
-		System.out.println(current.data);
+		System.out.print(current.data + " ");
+ 
+		System.out.println();
 	}
 
 //main function
@@ -121,7 +241,12 @@ public class LinkedListNew
 		ls.prepend(3);
 		ls.append(4);
 		ls.prepend(42);
-		ls.remove(10);
+		ls.print();
+
+		// ls.rotateLeft(5);
+		// ls.print();
+
+		ls.rotateRight(5);
 		ls.print();
 	}
 }
