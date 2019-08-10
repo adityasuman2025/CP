ef412aa992929320844c149c7c61582504467dfd
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-08-10 14:33:30 +0530

    iterative quicksort

--- Sorting/randomisedQuickSort.java
+++ Sorting/randomisedQuickSort.java
@@ -2,6 +2,45 @@ import java.util.*;
 
 class randomisedQuickSort
 {
+	static void iterativeQuickSort(int arr[], int l, int h)
+	{
+		if(l>=h)
+			return;
+
+	// Create an auxiliary stack and initialize top of stack 
+		int stack[] = new int[h-l+1];
+		int top = -1;
+
+	// push initial values of l and h to stack 	
+		stack[++top] = l;
+		stack[++top] = h;
+
+		while(top >= 0)
+		{
+			h = stack[top--];
+			l = stack[top--];
+
+			int p = randomPartition(arr, l, h);
+
+		// If there are elements on left side of pivot, 
+        // then push left side to stack 
+			if(p-1>l)
+			{
+				stack[++top] = l;
+				stack[++top] = p-1;
+			}
+
+
+        // If there are elements on right side of pivot, 
+        // then push right side to stack 
+	        if (p + 1 < h)
+	        { 
+	            stack[++top] = p + 1; 
+	            stack[++top] = h; 
+	        } 
+		}
+	}
+
 	static void quickSort(int arr[], int start, int end)
 	{
 		if(start>=end)
@@ -53,7 +92,7 @@ class randomisedQuickSort
 		int arr[] = {9,78,52,15,23,0,32, 2, 100, 81, 23};
 		System.out.println("Given array is: " + Arrays.toString(arr));	
 
-		quickSort(arr, 0, arr.length-1);
+		iterativeQuickSort(arr, 0, arr.length-1);
 		System.out.println("Sorted array is: " + Arrays.toString(arr));	
 	}
 }
