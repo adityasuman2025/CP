31f55ca26bbc86cc6bb76a1e8a784b6c0339b852
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-08-20 14:06:30 +0530

    nCr

---
+++ NCR.java
@@ -0,0 +1,17 @@
+class NCR 
+{ 	  
+	static int comb(int n , int r)
+	{
+		if( r== 0 || n == r)
+			return 1;
+		else
+			return comb(n-1,r)+comb(n-1,r-1);
+	}
+
+	// Driver code 
+	public static void main(String[] args) 
+	{ 
+	    int n = 23, r = 2; 
+	    System.out.println(comb(n, r)); 
+	} 
+} 
