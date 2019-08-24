df658688460a30e60f48336af98195cc3eb2f700
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-08-24 13:37:39 +0530

    PathInMatrixWithKCoins

--- Greedy/PathInMatrixWithKCoins.java
+++ Greedy/PathInMatrixWithKCoins.java
@@ -34,7 +34,7 @@ Explanation:
 Testcase 2: There are 2 possible paths with exactly K coins, which are (1 + 4 + 3 + 2 + 1) and (1 + 2 + 6 + 2 + 1).
 */
 
-class PathInMatrixWithKCoins
+class GFG
 {
     static int c = 0;
     static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -44,34 +44,43 @@ class PathInMatrixWithKCoins
 	    for(int a=0; a<T; a++)
 	    {
 	        int K = Integer.parseInt(bf.readLine().trim());
-	        
 	        int N = Integer.parseInt(bf.readLine().trim());
 	        String str[] = bf.readLine().split(" ");
 	        
             c = 0;
-            int sum = 0;
-            
-            solve(str, N, 0, 0, sum, K);
+            solve(str, N, 0, 0, K-Integer.parseInt(str[0]));
             System.out.println(c);
 	    }
 	}
 	
-	static void solve(String arr[], int N, int i, int j, int sum, int K)
+	static void solve(String arr[], int N, int i, int j, int sum)
 	{
-	    sum = sum+ Integer.parseInt(arr[i*N+j]);
-	    if(sum>K)
+	    //System.out.println(sum);
+	    
+	    if(sum<0)
 	        return;
 	        
-	    if(i==N-1 && j==N-1 && sum==K)
+	    if(i==N-1 && j==N-1 && sum==0)
         {
             c++;
             return;
         }
         
-        if(i+1<N && j<N)
-            solve(arr, N, i+1, j, sum, K);
+        if(i+1<N && arr[(i+1)*N+j] != "-1" && sum-Integer.parseInt(arr[(i+1)*N+j])>=0)
+        {
+            String temp = arr[(i+1)*N+j];
+            arr[(i+1)*N+j] = "-1";
+            
+            solve(arr, N, i+1, j, sum-Integer.parseInt(temp));
+            arr[(i+1)*N+j] = temp;
+        }
         
-        if(i<N && j+1<N)
-            solve(arr, N, i, j+1, sum, K);
+        if(j+1<N && arr[i*N+(j+1)] != "-1" && sum-Integer.parseInt(arr[i*N+(j+1)])>=0)
+        {
+            String temp = arr[i*N+(j+1)];
+            arr[i*N+(j+1)] = "-1";
+            solve(arr, N, i, j+1, sum-Integer.parseInt(temp));
+            arr[i*N+(j+1)] = temp;
+        }
 	}
 }
