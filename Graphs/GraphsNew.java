721bd3826e79f5ba81bdf24cd4cb1594be1f71f5
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-09-08 20:34:04 +0530

    GRAPHS QUIT

--- Graphs/GraphsNew.java
+++ Graphs/GraphsNew.java
@@ -1,21 +1,55 @@
-import java.util.*
+import java.util.*;
 
 class NewGraph
 {
-	int V; //no of vertex
-	LinkedList<Integer> list[]; //Adjacency Lists
+	private int V;   // No. of vertices 
+    private LinkedList<Integer> adj[]; //Adjacency Lists 
+  
+    // Constructor 
+    NewGraph(int v) 
+    { 
+        V = v; 
+        adj = new LinkedList[v]; 
+        for (int i=0; i<v; ++i) 
+            adj[i] = new LinkedList<>(); 
+    } 
+  
+    // Function which adds an edge from v -> w 
+    void addEdge(int v,int w) 
+    { 
+        adj[v].add(w); 
+    } 
 
-	NewGraph(int V)
+	void print()
 	{
-		this.V = V;
-		list[] = new LinkedList[V];
-
 		for(int i=0; i<V; i++)
-			list[i] = new LinkedList<>();
+		{
+			System.out.println("Adjacency list of vertex " + i + " is: ");
+			for(Integer vertex: adj[i])
+			{
+				System.out.print(vertex + ", "); 
+			}
+			System.out.println(""); 
+		}
 	}
 }
 
 class GraphsNew
 {
+	public static void main(String[] args) 
+	{
+		int V = 6;
+		NewGraph graph = new NewGraph(V);
 
+		graph.addEdge(0, 1); 
+        graph.addEdge(0, 2); 
+        graph.addEdge(1, 3); 
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+  		graph.addEdge(4, 5);
+        graph.addEdge(3, 5);
+  		
+  		graph.print();
+	}
 }
