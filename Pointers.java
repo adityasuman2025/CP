92b2ab751d93ba989452a5071533a34224e9e648
adityasuman2025 <aditya.me16@iitp.ac.in>
2019-09-11 19:35:48 +0530

    pointers

---
+++ Pointers.java
@@ -0,0 +1,14 @@
+class Pointers
+{
+	public static void main(String[] args) {
+		int a = 10;
+
+		increment(a);
+		System.out.println(a);
+	}
+
+	static void increment(int a)
+	{
+		a++;
+	}
+}
--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
-
 using namespace std;
+
+void workingIncrement(int *a)
+{
+	*a = *a + 1;
+	cout<<"address of a in increment function "<<&a<<endl;
+}
+
+void notWorkingIncrement(int a)
+{
+	a = a + 1;
+}
+
+int sumArray(int *A, int size)
+{	
+	int sum = 0;
+	for(int i=0; i<size; i++)
+	{
+		sum += A[i];
+	}
+
+	return sum;
+}
+
+void Double(int *A, int size)
+{	
+	//int sum = 0;
+	for(int i=0; i<size; i++)
+	{
+		A[i] = 2*A[i];
+	}
+}
+
+void print2dArray(int *arr[], int N)
+{
+	for(int i = 0;i<N;i++)
+    {
+        for(int j = 0;j<N;j++)
+        {
+            cout<<arr[i][j]<<" "; // *(*(arr+i)+j)
+        }
+        cout << endl;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
@@ -65,6 +107,68 @@ int main(int argc, char const *argv[])
 
 	***r = **q + 9; //changing value of b
 	cout<<b<<endl; //value of b
-	
+
+//pointers as function arguments
+	int a = 10;
+	//a++;
+	workingIncrement(&a);
+	//notWorkingIncrement(a);
+	cout<<"address of a in main function "<<&a<<endl;
+	cout<<a<<endl;
+	cout<<endl;
+
+//arrays & pointers
+	int A[] = {1,2,3,4,5};
+
+	cout<<*A<<endl; //value of first element of array A
+	cout<<A[0]<<endl; //value of first element of array A
+	cout<<&A<<endl; //address of first element of array A
+	cout<<A<<endl; //address of first element of array A
+
+	for(int i=1; i<5; i++)
+	{
+		cout<<*(A+i)<<endl; //value of i element of array A
+		cout<<A[i]<<endl; //value of i element of array A
+		cout<<&A[i]<<endl; //address of first element of array A
+		cout<<A+i<<endl; //address of i element of array A		
+	}
+	cout<<endl;
+
+	int size = sizeof(A)/sizeof(A[0]);
+
+	cout<<"size of array: "<<size<<endl;
+	cout<<"sum of elements: "<<sumArray(&A[0], size)<<endl;
+
+
+	Double(A, size);
+	for(int i=0; i<5; i++)
+	{
+		cout<<*(A+i)<<" "; //value of i element of array A
+	}
+	cout<<endl;
+
+//character array 
+	char str[] = "Bro";
+	char *s = str;
+	s[0] = 'V';
+	cout<<*(s+2)<<endl; //same is s[2] //same as (s+2)
+	cout<<s<<endl; //same as str
+ 
+//2D arrays
+	int arr[2][3] = {{1,2,3}, {4,5,6}};
+	cout<<arr<<endl; //address of first element of arr i.e arr[0][0] //same as &arr[0] or arr[0] or arr[0][0] or *arr
+	cout<<arr+1<<endl; //address of 2nd element of arr i.e arr[1][0] //same as &arr[1] or arr[1] or *(arr+1)
+	cout<<(arr+1)+2<<endl; //address of  arr[1][2] //same as arr[1]+2
+	cout<<*(*(arr+1)+2)<<endl; //value of  arr[1][2] //same as arr[1]+2
+	cout<<*(*(arr)+1)<<endl; //2
+
+	//B[i][j] = *(B[i]+j) = *(*(B+i)+j)
+
+//const
+	const int* bro = &b; 
+	cout<<*bro<<endl;
+	//*bro = 17; //it will give error as pointer bro is constant
+	cout<<b<<endl;
+
 	return 0;
 }
