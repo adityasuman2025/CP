7f9b6fe5469dda0b839a11f9228e4a7d139ab3f2
adityasuman2025 <adityasuman2025@gmail.com>
2021-08-10 08:57:16 +0530

    revisiosn

--- bhemu_jee/bhemu_jee.java
+++ bhemu_jee/bhemu_jee.java
@@ -3,8 +3,9 @@ class bhemu_jee {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // for(int a = 2; a<=10; a+=2) {
-        System.out.println(a);
-        // }
+        int b = a;
+        for(; b<=a*10; b+=a) {
+        System.out.println(b);
+        }
     }
 }
