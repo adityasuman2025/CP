55f1407d64048f9778e1e938efc68f8a3ca55136
adityasuman2025 <adityasuman2025@gmail.com>
2021-08-10 08:39:45 +0530

    dione

---
+++ bhemu_jee/bhemu_jee.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class bhemu_jee {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // for(int a = 2; a<=10; a+=2) {
+        System.out.println(a);
+        // }
+    }
+}
