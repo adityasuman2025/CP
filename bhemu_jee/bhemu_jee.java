1811ea4d8268553bc629dec066419f8af9930f8e
adityasuman2025 <adityasuman2025@gmail.com>
2021-08-21 10:09:39 +0530

    oop

--- bhemu_jee/bhemu_jee.java
+++ bhemu_jee/bhemu_jee.java
@@ -1,11 +1,28 @@
-import java.util.Scanner;
+class Scanner {
+    // Scanner() {
+    //     System.out.println("Scanner");
+    // }
+
+    public void nextInt(int no) {
+        System.out.println("int is " + no);
+    }
+
+    public void nextFloat(int no) {
+        System.out.println("float is " + no);
+    }
+}
+
 class bhemu_jee {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = a;
-        for(; b<=a*10; b+=a) {
-        System.out.println(b);
-        }
+    public static void main(String args[]) {
+        bhemu_jee bheBhe = new bhemu_jee();
+        bheBhe.anything();
+
+        Scanner scan = new Scanner();
+        scan.nextInt(40);
+        scan.nextFloat(42);
+    }
+
+    public void anything() {
+        System.out.println("anything");
     }
 }
