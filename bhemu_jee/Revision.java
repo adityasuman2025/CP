688fef4be046862b2b617a4cdf59204c3a29af85
adityasuman2025 <adityasuman2025@gmail.com>
2021-08-26 13:08:04 +0530

    aman revision class

--- bhemu_jee/Revision.java
+++ bhemu_jee/Revision.java
@@ -1,14 +1,53 @@
 class Revision {
     public static void main(String [] args) {
-        int a = 1;
-        int b = 10;
+        // int a = 1;
+        // int b= 5;
+        // int cloneA = a;
+        // a = b;
+        // b = cloneA;
+        // System.out.println(a);
+        // System.out.println(b);
+        /////////////////////////////////////////////
 
-        int sum = 0;
-        for ( ; a<=b; a++) {
-            sum = sum + a;
-            // sum += a;
+        // Revision ka = new Revision();
+        // int ans=ka.multi(a,b);
+        // System.out.println(ans);
+        ////////////////////////////////////////////
+       
+        // int a[] = new int [10];
+        // for(int i = 0; i <= a.length; i++) {
+        //     a[i]=i;
+        //     System.out.println(a[i]);
+        // }
+        ////////////////////////////////////////////
+
+        // int arr[][] = new int[3][3];
+        // for(int i = 0; i < arr.length; i++){
+        //     int a = 0;
+        //     for( ; a < arr.length; a++){
+        //         arr[i][a] = (3*i + 1)+a;  
+        //         System.out.print(arr[i][a] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        ////////////////////////////////////////////
+        for(int a = 1; a <= 10; a++) {
+            for(int i = 1; i<= 10 - a; i++){
+                System.out.print(" ");
+            }
+
+            for(int i = 1; i <= a; i++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
-        System.out.println(sum);
     }
+
+    // public int multi(int a, int b){
+    //     int guna = 1;
+    //     for( ; a<=b; a++) {
+    //         guna = guna * a;
+    //     }
+    //     return guna;
+    // }
 }
