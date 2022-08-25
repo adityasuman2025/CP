e605dd11a022dc5797b7705d1d8caab58c1048aa
adityasuman2025 <adityasuman2025@gmail.com>
2022-08-25 21:19:05 +0530

    some maths calulations in js

---
+++ JS/mathFormulas.js
@@ -0,0 +1,149 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let inputStringArr = [];
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputStringArr = inputString.split('\n');
+
+    main();
+});
+
+function main() {
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    // ws.write(result + '\n');
+    // ws.end();
+
+    const number = parseInt(inputStringArr[0]);
+    const prime = parseInt(inputStringArr[1]);
+
+    // const powerOfPrime = getPowerOfPrime(number, prime);
+    // console.log("powerOfPrime", powerOfPrime, inputStringArr);
+
+    // const binary = decimalIntoBinary(number);
+    // console.log("binary", binary, inputStringArr);
+
+    // const factorialOfNumber = factorial(number);
+    // console.log("factorialOfNumber", factorialOfNumber, inputStringArr);
+
+    // const fibonacciNoAtPos = fibonacciNoAtPosition(number);
+    // console.log("fibonacciNoAtPos", fibonacciNoAtPos, inputStringArr);
+
+    // const fibonacciSeries = [];
+    // fibonacciSeriesTillPosition(fibonacciSeries, number);
+    // console.log("fibonacciSeries", fibonacciSeries);
+
+    const isNoPrime = isPrime(number);
+    console.log("isNoPrime", isNoPrime);
+
+    // const primeNosTillNo = primeTillNumber(number);
+    // console.log("primeNosTillNo", primeNosTillNo);
+}
+
+function getPowerOfPrime(number, prime) {
+    let power = 0;
+    while (number % prime == 0) {
+        power ++;
+        number = number/prime;
+    }
+
+    return power;
+}
+
+function decimalIntoBinary(number) {
+    let binary = "";
+
+    while (number >= 2) {
+        let remainder = number % 2;
+        binary = remainder + binary;
+
+        number = parseInt(number/2);
+    }
+    binary = number + binary;
+
+    return binary;
+}
+
+function factorial(number) {
+    if ([0, 1].includes(number)) {
+        return 1;
+    }
+
+    return number * factorial(number - 1);
+}
+
+function fibonacciNoAtPosition(position) {
+    if ([1, 2].includes(position)) {
+        return position - 1;
+    }
+
+    return fibonacciNoAtPosition(position - 1) + fibonacciNoAtPosition(position - 2);
+}
+
+function fibonacciSeriesTillPosition(fibonacciSeries, position) {
+    if ([1, 2].includes(position)) {
+        fibonacciSeries[position - 1 ] = position - 1;
+        return position - 1;
+    }
+
+    if (fibonacciSeries[position - 1]) {
+        return fibonacciSeries[position - 1] ;
+    }
+
+    let thisPosFibo = fibonacciSeriesTillPosition(fibonacciSeries, position - 1) + fibonacciSeriesTillPosition(fibonacciSeries, position - 2);
+    fibonacciSeries[position - 1] = thisPosFibo;
+    return thisPosFibo;
+}
+
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+
+    if (number < 4) {
+        return true;
+    }
+
+    if ((number%2 == 0) || (number%3 == 0)) {
+        return false;
+    }
+
+    for (let i = 5; i * i <= number; i = i + 6) {
+        if ((number % i == 0) || (number % (i+2) == 0)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function primeTillNumber(number) {
+    let primes = [];
+
+    if (number > 1) {
+        primes = [2];
+    }
+
+    if (number > 2) {
+        primes = [2, 3];
+    }
+
+    for (let i = 5; i < number;) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+
+        if ((i+2 < number) && isPrime(i + 2)) {
+            primes.push(i + 2);
+        }
+
+        i += 6;
+    }
+
+    return primes;
+}
