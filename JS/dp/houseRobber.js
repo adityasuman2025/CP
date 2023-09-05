4f1268ae390a3e280bb607319f4f1b08faf7f2f5
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-05 20:56:55 +0530

    some dp qstns

--- JS/dp/houseRobber.js
+++ JS/dp/houseRobber.js
@@ -32,23 +32,23 @@ Total amount you can rob = 2 + 9 + 1 = 12.
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+var rob = function (nums) {
     let dp = [];
     dp[0] = nums[0];
-    
-    for (let i = 1; i<nums.length; i++) {
-        if (i==1) {
-            dp[i] = Math.max(nums[i], dp[i-1]);
+
+    for (let i = 1; i < nums.length; i++) {
+        if (i == 1) {
+            dp[i] = Math.max(nums[i], dp[i - 1]);
             //for second house
             //1. if he robs this house then we can't include his first robbery
             //2. if he not robs this house then we can't include his first robbery
         } else {
-            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]); 
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
             //either rob this house or not
             //1. if robbing this house then we can also include max robbery till prev to prev house (i-2) 
             //2. if not robbing this house then we can include max robbery till prev house (i-1)
         }
     }
-    
-    return dp[nums.length-1]
+
+    return dp[nums.length - 1]
 };
