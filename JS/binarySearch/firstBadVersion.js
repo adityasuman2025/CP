8685c982054715f25298b15eec7d34028a500b7d
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-08 19:08:23 +0530

    bs qstns

---
+++ JS/binarySearch/firstBadVersion.js
@@ -0,0 +1,63 @@
+/*
+https://leetcode.com/problems/first-bad-version/description/
+
+278. First Bad Version
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ 
+
+Example 1:
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+Example 2:
+
+Input: n = 1, bad = 1
+Output: 1
+*/
+
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let start = 1, end = n, minBadVer = n;
+        while (start <= end) {
+            let mid = parseInt((start + end) / 2);
+
+            if (isBadVersion(mid) === true) {
+                minBadVer = Math.min(n, mid);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return minBadVer;
+    };
+};
