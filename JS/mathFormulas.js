9a0a3525558d449e5a51d81e5f8a6ab389356d29
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-09 17:19:52 +0530

    math formulas

--- JS/mathFormulas.js
+++ JS/mathFormulas.js
@@ -1,145 +1,129 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let inputStringArr = [];
+// let inputString = '';
+// let inputStringArr = [];
 
-process.stdin.on('data', function (inputStdin) {
-    inputString += inputStdin;
-});
+// process.stdin.on('data', function (inputStdin) {
+//     inputString += inputStdin;
+// });
 
-process.stdin.on('end', function () {
-    inputStringArr = inputString.split('\n');
+// process.stdin.on('end', function () {
+//     inputStringArr = inputString.split('\n');
 
-    main();
-});
+//     main();
+// });
 
-function main() {
-    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-    // ws.write(result + '\n');
-    // ws.end();
+// function main() {
+//     // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+//     // ws.write(result + '\n');
+//     // ws.end();
 
-    const number = parseInt(inputStringArr[0]);
-    const prime = parseInt(inputStringArr[1]);
+//     const number = parseInt(inputStringArr[0]);
+//     const prime = parseInt(inputStringArr[1]);
 
-    // const powerOfPrime = getPowerOfPrime(number, prime);
-    // console.log("powerOfPrime", powerOfPrime, inputStringArr);
+// }
 
-    // const factorialOfNumber = factorial(number);
-    // console.log("factorialOfNumber", factorialOfNumber, inputStringArr);
+function gcd(a, b) {
+    if (!b) return a;
 
-    // const fibonacciNoAtPos = fibonacciNoAtPosition(number);
-    // console.log("fibonacciNoAtPos", fibonacciNoAtPos, inputStringArr);
-
-    // const fibonacciSeries = [];
-    // fibonacciSeriesTillPosition(fibonacciSeries, number);
-    // console.log("fibonacciSeries", fibonacciSeries);
-
-    // const isNoPrime = isPrime(number);
-    // console.log("isNoPrime", isNoPrime);
-
-    // const primeNosTillNo = primeTillNumber(number);
-    // console.log("primeNosTillNo", primeNosTillNo);
-
-    const prFactor = primeFactors(number);
-    console.log("prFactor", prFactor);
+    gcd(b, a % b);
 }
 
+
+// index: 0 1 2 3
+// value: 0 1 1 2
+function fibbonaci(n) {
+    let memo = [0, 1, 1];
+
+    function fibb(n) {
+        if (n < 2) return n;
+        if (n < 3) return n - 1;
+
+        const f = memo.hasOwnProperty(n - 1) ? memo[n - 1] : fibb(n - 1);
+        const s = memo.hasOwnProperty(n - 2) ? memo[n - 2] : fibb(n - 2);
+        memo[n] = f + s;
+
+        return memo[n];
+    }
+    fibb(n);
+
+    return memo[n];
+}
+let ans = fibbonaci(10);
+// console.log("fibbonaci", ans)
+
+
+// n! = n * (n-1)!
+function factorial(n) {
+    let memo = [1, 1];
+
+    function fact(n) {
+        if (n < 2) return 1;
+
+        const prev = memo.hasOwnProperty(n - 1) ? memo[n - 1] : fact(n - 1);
+        memo[n] = n * prev;
+        return memo[n]
+    }
+    fact(n);
+
+    return memo[n];
+}
+let ans1 = factorial(5);
+// console.log("factorial", ans1)
+
+
+// get power of a prime no in number
 function getPowerOfPrime(number, prime) {
-    let power = 0;
-    while (number % prime == 0) {
-        power++;
-        number = number / prime;
+    let c = 0;
+
+    while (number > 1) {
+        number = parseInt(number / prime);
+        c++;
     }
 
-    return power;
+    return c;
 }
+let ans2 = getPowerOfPrime(81, 3);
+// console.log("getPowerOfPrime", ans2)
 
-function factorial(number) {
-    if ([0, 1].includes(number)) return 1;
 
-    return number * factorial(number - 1);
-}
+// checks if a no is prime
+function isPrime(n) {
+    if (n < 2) return false;
+    if (n < 4) return true;
 
-function fibonacciNoAtPosition(position) {
-    if ([1, 2].includes(position)) return position - 1;
+    if ((n % 2 === 0) || (n % 3 === 0)) return true;
 
-    return fibonacciNoAtPosition(position - 1) + fibonacciNoAtPosition(position - 2);
-}
-
-function fibonacciSeriesTillPosition(fibonacciSeries, position) {
-    if ([1, 2].includes(position)) {
-        fibonacciSeries[position - 1] = position - 1;
-        return position - 1;
-    }
-
-    if (fibonacciSeries[position - 1]) {
-        return fibonacciSeries[position - 1];
-    }
-
-    let thisPosFibo = fibonacciSeriesTillPosition(fibonacciSeries, position - 1) + fibonacciSeriesTillPosition(fibonacciSeries, position - 2);
-    fibonacciSeries[position - 1] = thisPosFibo;
-    return thisPosFibo;
-}
-
-function isPrime(number) {
-    if (number < 2) {
-        return false;
-    }
-
-    if (number < 4) {
-        return true;
-    }
-
-    if ((number % 2 == 0) || (number % 3 == 0)) {
-        return false;
-    }
-
-    for (let i = 5; i * i <= number; i = i + 6) {
-        if ((number % i == 0) || (number % (i + 2) == 0)) {
-            return false;
-        }
+    for (let i = 5; i * 1 <= n; i += 6) {
+        if ((n % i === 0) || (n % (i + 2) === 0)) return false;
     }
 
     return true;
 }
 
+
+// 2 3 5 7 11 13 17 19...
 function primeTillNumber(number) {
     let primes = [];
 
-    if (number > 1) {
-        primes = [2];
-    }
+    if (number > 1) primes = [2];
 
-    if (number > 2) {
-        primes = [2, 3];
-    }
+    if (number > 2) primes = [2, 3];
 
-    for (let i = 5; i < number;) {
-        if (isPrime(i)) {
-            primes.push(i);
-        }
+    for (let i = 5; i < number; i += 6) {
+        if (isPrime(i)) primes.push(i);
 
-        if ((i + 2 < number) && isPrime(i + 2)) {
-            primes.push(i + 2);
-        }
-
-        i += 6;
+        if ((i + 2 < number) && isPrime(i + 2)) primes.push(i + 2);
     }
 
     return primes;
 }
 
+
+// function to get prime factors of a number
 function primeFactors(number) {
     let factors = {};
-
-    // const primNos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 35, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
-    // primNos.forEach(primeNo => {
-    //     while (number % primeNo == 0) {
-    //         factors[primeNo] = factors[primeNo] ? (factors[primeNo] + 1): 1;
-    //         number = number / primeNo;
-    //     }
-    // });
 
     let i = 2;
     while (number % i == 0) {
@@ -154,15 +138,7 @@ function primeFactors(number) {
         }
     }
 
-    if (number > 2) {
-        factors[number] = 1;
-    }
+    if (number > 2) factors[number] = 1;
 
     return factors;
-}
-
-function gcd(a, b) {
-    if (b === 0) return a;
-
-    return gcd(b, a % b);
 }
