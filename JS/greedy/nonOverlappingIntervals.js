0e2a4f66a7fe192879632c6c93e526a775dd13a1
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-10 14:18:29 +0530

    non overlapping interval

--- JS/greedy/nonOverlappingIntervals.js
+++ JS/greedy/nonOverlappingIntervals.js
@@ -23,3 +23,23 @@ Output: 0
 Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
 */
 
+// this is similar to activity selection problem
+var eraseOverlapIntervals = function (intervals) {
+    if (!intervals.length) return 0;
+
+    // Sort the intervals by end time
+    intervals.sort((a, b) => a[1] - b[1]);
+    console.log("intervals", intervals)
+
+    let nonOverlapCount = 1;
+    let endTime = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] >= endTime) {
+            nonOverlapCount++;
+            endTime = intervals[i][1];
+        }
+    }
+
+    return intervals.length - nonOverlapCount;
+};
