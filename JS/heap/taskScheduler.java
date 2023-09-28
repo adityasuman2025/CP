76213677444a03d9ed2be01d1e39e77658f3ec9a
adityasuman2025 <adityasuman2025@gmail.com>
2023-09-28 07:18:50 +0530

    task scheduler

---
+++ JS/heap/taskScheduler.java
@@ -0,0 +1,72 @@
+/*
+https://leetcode.com/problems/task-scheduler
+
+621. Task Scheduler
+Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+
+Return the least number of units of times that the CPU will take to finish all the given tasks.
+
+ 
+
+Example 1:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+Output: 8
+Explanation: 
+A -> B -> idle -> A -> B -> idle -> A -> B
+There is at least 2 units of time between any two same tasks.
+Example 2:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 0
+Output: 6
+Explanation: On this case any permutation of size 6 would work since n = 0.
+["A","A","A","B","B","B"]
+["A","B","A","B","A","B"]
+["B","B","B","A","A","A"]
+...
+And so on.
+Example 3:
+
+Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+Output: 16
+Explanation: 
+One possible solution is
+A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+*/
+
+// ref: https://www.youtube.com/watch?v=s8p8ukTyA2I
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        pq.addAll(map.values());
+        
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        int time = 0;
+
+        while (!pq.isEmpty() || !q.isEmpty()) {
+            time++;
+
+            if (!pq.isEmpty()) {
+                int cnt = pq.poll() - 1;
+
+                if (cnt != 0) {
+                    q.offer(new Pair(cnt, time + n));
+                }
+            }
+
+            if (!q.isEmpty() && q.peek().getValue() == time) {
+                pq.offer(q.poll().getKey());
+            }
+        }
+
+        return time;
+    }
+}
