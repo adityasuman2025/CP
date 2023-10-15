88b599c86302c0b2519aca03bd44af26161326c3
adityasuman2025 <adityasuman2025@gmail.com>
2023-10-15 18:07:50 +0530

    priortiy queue updated

--- JS/heap/priorityQueue.js
+++ JS/heap/priorityQueue.js
@@ -9,104 +9,126 @@
 */
 
 
-class QElement {
-    constructor(val, priority) {
-        this.val = val;
-        this.priority = priority;
-    }
-}
-
 class PriorityQueue {
-    constructor() {
-        this.items = [];
+    /**
+     * @param {(a: any, b: any) => -1 | 0 | 1} compare - 
+     * compare function, similar to parameter of Array.prototype.sort
+     */
+    constructor(compare) {
+        this.data = [];
+        this.compare = compare;
     }
 
-    enqueue(val, priority) {
-        let newItem = new QElement(val, priority);
-        let hasBeenAdded = false;
+    /**
+     * return {number} amount of items
+     */
+    size() {
+        return this.data.length;
+    }
 
-        // iterating through the entire item array to add element at the correct location of the Queue
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].priority > priority) {
-                // Once the correct location is found newItem is put there
-                this.items.splice(i, 0, newItem); // splice is used to insert new item at any specific index in array // ref: https://www.freecodecamp.org/news/insert-into-javascript-array-at-specific-index/
+    /**
+     * returns the head element
+     */
+    peek() {
+        return this.data[0];
+    }
 
-                hasBeenAdded = true;
+    /**
+     * @param {any} element - new element to add
+     */
+    add(element) {
+        this.data.push(element);
+
+        this.heapifyUp();
+    }
+
+    /**
+     * remove the head element
+     * @return {any} the head element
+     */
+    poll() {
+        let val = this.data[0];
+
+        this.data[0] = this.data[this.data.length - 1];
+        this.data.pop();
+        this.heapifyDown();
+
+        return val;
+    }
+
+    heapifyUp() {
+        let arr = this.data;
+        let idx = this.data.length - 1;
+
+        while (this.getParentIdx(idx) >= 0 && (this.compare(arr[idx], arr[this.getParentIdx(idx)]) < 0)) {
+            this.swap(idx, this.getParentIdx(idx));
+            idx = this.getParentIdx(idx);
+        }
+    }
+
+    heapifyDown() {
+        let n = this.data.length, arr = this.data;
+        let idx = 0;
+
+        while (this.getLeftChildIdx(idx) < n || this.getRightChildIdx(idx) < n) {
+            let smallerEle = arr[this.getLeftChildIdx(idx)], smallerEleIdx = this.getLeftChildIdx(idx);
+
+            if (this.getRightChildIdx(idx) < n && (this.compare(arr[this.getRightChildIdx(idx)], smallerEle) < 0)) {
+                smallerEle = arr[this.getRightChildIdx(idx)];
+                smallerEleIdx = this.getRightChildIdx(idx);
+            }
+
+            if (this.compare(smallerEle, arr[idx]) < 0) {
+                this.swap(idx, smallerEleIdx);
+                idx = smallerEleIdx;
+            } else {
                 break;
             }
         }
-
-        // if the element have the highest priority, then it is added/pushed at the end of the queue
-        if (!hasBeenAdded) this.items.push(newItem);
     }
 
-    dequeue() {
-        if (this.items.length === 0) return null;
 
-        return this.items.shift(); // removing and returning the first element of the priority queue
+    getParentIdx(i) {
+        return parseInt((i - 1) / 2);
     }
 
-    front() {
-        if (this.items.length === 0) return null;
-
-        return this.items[0];
+    getLeftChildIdx(i) {
+        return 2 * i + 1;
     }
 
-    rear() {
-        if (this.items.length === 0) return null;
-
-        return this.items[this.items.length - 1];
+    getRightChildIdx(i) {
+        return 2 * i + 2;
     }
 
-    isEmpty() {
-        return this.items.length === 0;
-    }
-
-    print() {
-        let res = "";
-        for (let i = 0; i < this.items.length; i++) {
-            res += (this.items[i].val + " ")
-        }
-
-        console.log("print:", res);
+    swap(i, j) {
+        let temp = this.data[i];
+        this.data[i] = this.data[j];
+        this.data[j] = temp;
     }
 }
 
-// creating object for queue class
-var priorityQueue = new PriorityQueue();
 
-// testing isEmpty and front on an empty queue
-// return true
-console.log(priorityQueue.isEmpty());
+const minHeap = new PriorityQueue((a, b) => a - b)
+minHeap.add(5)
+minHeap.add(3)
+minHeap.add(1)
+minHeap.add(4)
+minHeap.add(2)
+const result1 = []
+while (minHeap.size() > 0) {
+    result1.push(minHeap.poll())
+}
+console.log("result1", result1);
 
-// returns "No items in Queue"
-console.log(priorityQueue.front());
 
-// adding items to the queue
-priorityQueue.enqueue("Sumit", 2);
-priorityQueue.enqueue("Gourav", 1);
-priorityQueue.enqueue("Piyush", 1);
-priorityQueue.enqueue("Sunny", 2);
-priorityQueue.enqueue("Sheru", 3);
-
-// prints [Gourav Piyush Sumit Sunny Sheru]
-priorityQueue.print();
-
-// prints Gourav
-console.log(priorityQueue.front().val);
-
-// prints Sheru
-console.log(priorityQueue.rear().val);
-
-// removes Gouurav
-console.log(priorityQueue.dequeue().val);
-
-// priorityQueue contains
-// [Piyush Sumit Sunny Sheru]
-priorityQueue.print();
-
-// Adding another element to the queue
-priorityQueue.enqueue("Sunil", 2);
-
-// prints [Piyush Sumit Sunny Sunil Sheru]
-priorityQueue.print();
+const maxHeap = new PriorityQueue((a, b) => b - a)
+maxHeap.add(5)
+maxHeap.add(3)
+maxHeap.add(1)
+maxHeap.add(4)
+maxHeap.add(2)
+const result2 = []
+while (maxHeap.size() > 0) {
+    result2.push(maxHeap.poll())
+}
+console.log("result2", result2);
