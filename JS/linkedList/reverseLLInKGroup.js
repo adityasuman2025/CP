5c3d7666a2c17191c7a5ad286c00a619df2c8ce7
adityasuman2025 <adityasuman2025@gmail.com>
2023-10-20 08:29:54 +0530

    Reverse Nodes in k-Group in linkedlist

--- JS/linkedList/reverseLLInKGroup.js
+++ JS/linkedList/reverseLLInKGroup.js
@@ -22,6 +22,7 @@ Input: head = [1,2,3,4,5], k = 3
 Output: [3,2,1,4,5]
 */
 
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -29,37 +30,39 @@ Output: [3,2,1,4,5]
  *     this.next = (next===undefined ? null : next)
  * }
  */
-
-
 /**
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
-*/
+ */
+// time: O(n), space: O(1)
 var reverseKGroup = function(head, k) {
     if (!head) return head;
-    
-    let temp = head, size = 0;
+
+    let temp = head, length = 0;
     while (temp) {
+        length++;
         temp = temp.next;
-        size++;
     }
-    if (k>size) return head;
-    
-    let curr = head;
-    let prev = null, next = null;
-    
-    let i = 0;
-    while (i<k && curr) {
-        next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        
-        curr = next;
-        i++;
+
+    function util(head, leftLength) {
+        if (!head) return head;
+
+        if (k > leftLength) return head;
+
+        let c = k;
+        let prev = null, next = null, curr = head;
+        while (curr && c--) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+
+            curr = next;
+        }
+        head.next = util(curr, leftLength - k);
+
+        return prev;
     }
-    
-    head.next = reverseKGroup(curr, k)
-    
-    return prev;
+
+    return util(head, length);
 };
