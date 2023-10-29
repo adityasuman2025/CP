0bf4530b2213d1b56d007fd54a5c4f39246168d6
adityasuman2025 <adityasuman2025@gmail.com>
2023-10-29 21:53:31 +0530

    angleBWHourAndMinuteHand

--- JS/maths/mathFormulas.js
+++ JS/maths/mathFormulas.js
@@ -142,3 +142,24 @@ function primeFactors(number) {
 
     return factors;
 }
+
+
+function angleBWHourAndMinuteHand() {
+    const [hour, min] = time.split(':').map((seg) => parseInt(seg))
+
+    const h = (hour >= 12 ? hour - 12 : hour)
+    const m = min
+
+    const angleMin = (m / 60) * 360
+    const angleHour = (h / 12) * 360 + angleMin / 12
+
+
+    const angle = Math.abs(angleHour - angleMin)
+    const finalAngle = angle > 180 ? 360 - angle : angle
+    return Math.round(finalAngle)
+}
+angle('12:00')
+// 0
+
+angle('23:30')
+// 165
