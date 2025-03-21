be5a749f2e2f4a7a0fba1d1545f6547fc8b8d456
Aditya Suman <adityasuman2025@gmail.com>
2025-03-21 07:41:34 +0530

    Update mergeNSortedArr.js

--- JS/sorting/mergeNSortedArr.js
+++ JS/sorting/mergeNSortedArr.js
@@ -23,59 +23,37 @@ merge(
  * @return {number[]} 
  */
 // time: O(nk), space: O(nk)
-function merge(arrList) {
-    let resp = [];
+function merge(arr) {
+  let len = arr.length;
 
-    let pointer = new Array(arrList.length).fill(0);
-    let totalLength = 0, isLengthDone = false;
+  let idxArr = new Array(len).fill(0);
+  let allLen = arr.reduce((acc, arr) => acc + arr.length, 0);
+  
+  let ans = [];
+  while (allLen) {
+    let minVal = Number.MAX_SAFE_INTEGER
+    let minValIdx = -1;
 
-    while (true) {
-        let minI, minJ;
-        for (let i = 0; i < arrList.length; i++) {
-            if (!isLengthDone) totalLength += arrList[i].length; // for storing the total no of elements in arrList
-
-            if (minI === undefined || minJ === undefined) {
-                if (pointer[i] < arrList[i].length) {
-                    minI = i;
-                    minJ = pointer[i];
-                }
-            } else {
-                if (arrList[i][pointer[i]] < arrList[minI][minJ]) {
-                    minI = i;
-                    minJ = pointer[i];
-                }
-            }
-        }
-        isLengthDone = true;
-
-        if ((minI === undefined || minJ === undefined)) break; // if all elements of the arrList has been picked into resp then breaking the loop
-
-        resp.push(arrList[minI][minJ]);
-        pointer[minI] = pointer[minI] + 1;
-
-        if (resp.length >= totalLength) break; // if all elements of the arrList has been picked into resp then breaking the loop
+    for (let i=0; i<len; i++) {
+      if (arr[i][idxArr[i]] && arr[i][idxArr[i]] < minVal) {
+        minVal = arr[i][idxArr[i]];
+        minValIdx = i;
+      }
     }
 
-    return resp;
+    ans.push(minVal);
+    idxArr[minValIdx] = idxArr[minValIdx] + 1;
+
+    allLen--;
+  }
+
+  return ans;
 }
 
-// const res = merge([
-//   [0, 3],[2, 4],[1, 5]
-// ]);
-// console.log(res);
-
-const res2 = merge([
-    [1], [], [2]
-]);
-console.log(res2);
-
-const res3 = merge([]);
-console.log(res3);
-
-const res4 = merge([
-    [1, 1, 1, 100, 1000, 10000],
-    [1, 2, 2, 2, 200, 200, 1000],
-    [1000000, 10000001],
-    [2, 3, 3]
-]);
-console.log(res4);
+let arr = [
+  [1,1,1,100,1000,10000],
+  [1,2,2,2,200,200,1000],
+  [1000000,10000001],
+  [2,3,3]
+];
+console.log(merge(arr))
