3d539b4ac70e881490ad92a01c34818e7a24dad9
adityasuman2025 <adityasuman2025@gmail.com>
2026-07-15 07:40:56 +0530

    serialize and deserialize binary tree

--- JS/tree/serializeDeserializeBT.js
+++ JS/tree/serializeDeserializeBT.js
@@ -1,89 +1,77 @@
 /*
-https://bigfrontend.dev/problem/serialize-and-deserialize-binary-tree
-
-110. serialize and deserialize binary tree
-
-Can you transform(serialize) a binary tree into a string and restore(deserialize) a binary tree from the string? Just like what JSON.stringify() and JSON.parse() do.
-
-For example, for a tree from 91. invert a binary tree
-
-
-BFE.dev would serialize it to [1,2,3,4,null,null,5,6,7,8,null,null,null,null,9]
-
-But there are more ways of doing it rather than above, any would be fine as long as your deserialize() and serialize() work as a pair.
+    https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
 */
 
-// This is the class for the node
-// you can use this directly as it is bundled with your code
-class Node {
-    constructor(val) {
-        this.value = val
-        this.left = null
-        this.right = null
-    }
-}
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
 
 /**
- * @param {Node} root
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
  * @return {string}
  */
-
-function serialize(root) {
+var serialize = function(root) {
     if (!root) return "";
-    let res = [root.value];
 
-    let q = [root];
-    while (q.length) {
-        let poll = q.shift();
+    const ans = [];
 
-        if (poll.left || poll.right) {
-            res.push(poll.left?.value || "null");
-            res.push(poll.right?.value || "null");
+    const q = [root];
+    let i = 0;
+    while (i < q.length) {
+        const node = q[i];
+        ans.push(node ? node.val : null);
+
+        if (node) {
+            q.push(node.left);
+            q.push(node.right);
         }
 
-        if (poll.left) q.push(poll.left)
-        if (poll.right) q.push(poll.right)
+        i++
     }
 
-    return res.join(",");
-}
+    return ans.join(",");
+};
 
 /**
- * @param {string} str
- * @return {Node}
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
  */
-function deserialize(str) {
-    if (!str) return null;
+var deserialize = function(data) {
+    if (!data) return null;
+    const arr = data.split(",");
 
-    const arr = str.split(",");
-    const length = arr.length;
+    let head = 0;
+    const root = new TreeNode(parseInt(arr[head]));
+    const q = [root];
 
-    function buildTree(nodeIdx) {
-        if ((nodeIdx >= length) || (arr[nodeIdx] === "null")) return null;
+    let childPointer = 1;
+    while (head < q.length && childPointer < arr.length) {
+        const curr = q[head++];
 
-        const node = new Node(arr[nodeIdx]);
-        node.left = buildTree(2 * nodeIdx + 1); // leftChildIdx = 2*nodeIdx + 1
-        node.right = buildTree(2 * nodeIdx + 2); // rightChildIdx = 2*nodeIdx + 2
+        if (arr[childPointer] && arr[childPointer] !== "null") {
+            curr.left = new TreeNode(parseInt(arr[childPointer]));
+            q.push(curr.left);
+        }
+        childPointer++;
 
-        return node;
+        if (arr[childPointer] && arr[childPointer] !== "null") {
+            curr.right = new TreeNode(parseInt(arr[childPointer]));
+            q.push(curr.right);
+        }
+        childPointer++;
     }
 
-    return buildTree(0);
-}
-
-
-const tree1 = '1,2,3,4,null,null,5,6,7,null,8,null,null,null,9';
-const tree2 = '1,2,3,4,null,null,5,6,7,8,null,null,null,null,9';
-
-const root1 = deserialize(tree1);
-const root2 = deserialize(tree2);
-console.log(root1);
-console.log(root2);
-
-const str1 = serialize(root1);
-const str2 = serialize(root2);
-console.log(str1);
-console.log(str2);
-
-console.log(str1 === tree1)//.toBe(true)
-console.log(str2 === tree2)//.toBe(true)
+    return root;
+};
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
